c579908122183b0d4dac778a44e9d83d0abab8da
Pure-Place <xiaobaishu_wxh@outlook.com>
2022-12-03 02:08:13 +0800

    更新 Test.cpp

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -5,14 +5,14 @@ using namespace std;
 
 int main()
 {
-	IdealFile F;
-	F.open("hosts");
-	cout << F.getbyline(2) << endl;
-	cout << F.getcontent() << "###@EOF" << endl;
-	F.addatline(2, "Testing!!!");
-	cout << F.getbyline(2) << endl;
-	F.save();
-	F.close();
+	IdealFile Test;
+	Test.open("hosts");
+	cout << Test.getbyline(2) << endl;
+	cout << Test.getcontent() << "###@EOF" << endl;
+	Test.addatline(2, "Testing!!!");
+	cout << Test.getbyline(2) << endl;
+	Test.save();
+	Test.close();
 	system("pause");
 	return 0;
 }
